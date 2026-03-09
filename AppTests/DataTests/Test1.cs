bdacdf34162b798818e5fdce9b973a057a193a6c
Kacper Adamczyk <254710@edu.p.lodz.pl>
2026-03-09 20:41:49 +0100

    Fixed test issue.

--- AppTests/DataTests/Test1.cs
+++ AppTests/DataTests/Test1.cs
@@ -1,4 +1,5 @@
 ﻿namespace DataTests
+{
 
     [TestClass]
     public class Test1
@@ -6,6 +7,7 @@
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(true);
+            Assert.AreEqual(1, 1);
         }
     }
+}
