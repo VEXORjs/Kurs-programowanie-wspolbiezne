fbce2d2db809fcdbba9ba79262a4d6fb10527c3d
Jakub <254912@edu.p.lodz.pl>
2026-03-10 10:47:17 +0100

    Loop for added

--- App/Data/Class1.cs
+++ App/Data/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace App
 {
@@ -7,6 +8,15 @@ namespace App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            List<string> lista =  new List<string>();
+            lista.Add("Jabłko");
+            lista.Add("Gruszka");
+            lista.Add("Banan");
+
+            for (int i = 0; i <lista.Count; i++) {
+                Console.WriteLine(lista[i]);
+            }
 
             Console.ReadKey();
         }
